5c8ef7cea2f00246aab60013ee7c0d2995e60fb8
ashukumar2001 <kumar.ashu414283@gmail.com>
2020-08-10 15:03:19 +0530

    Script for Number converter

---
+++ script/app.js
@@ -0,0 +1,57 @@
+var sel = document.getElementById("methods");
+var selected;
+var inputValue;
+var conversionName = sel.options[sel.selectedIndex].text;
+document.getElementById("conversion-name").innerHTML = conversionName + " :";
+
+document.getElementById("selected").addEventListener('click', function() {
+    selected = sel.options[sel.selectedIndex].value;
+    
+    if(selected === "hex2dec") { // Hexadecimal to Decimal
+        conversionName = sel.options[sel.selectedIndex].text;
+        document.getElementById("conversion-name").innerHTML = conversionName + " :";
+        document.getElementById("convert").addEventListener('click', function() {
+            inputValue = document.getElementById("input-value").value;
+            inputValue = inputValue.toString(); // inputValue converted to string because hex value give error
+            document.getElementById("output-value").value = hex2dec(inputValue); // printing output
+        });
+    } else if(selected === "dec2bin") { // Decimal to Binary
+        conversionName = sel.options[sel.selectedIndex].text;
+        document.getElementById("conversion-name").innerHTML = conversionName + " :";
+        document.getElementById("convert").addEventListener('click', function() {
+            inputValue = document.getElementById("input-value").value;
+            document.getElementById("output-value").value = dec2bin(inputValue); // printing output
+        });
+    }else if(selected === "bin2dec") { // Binary to Decimal
+        conversionName = sel.options[sel.selectedIndex].text;
+        document.getElementById("conversion-name").innerHTML = conversionName + " :";
+        document.getElementById("convert").addEventListener('click', function() {
+            inputValue = document.getElementById("input-value").value;
+            document.getElementById("output-value").value = bin2dec(inputValue); // printing output
+        });
+    }else if(selected === "dec2hex") { // Decimal to Hexadecimal
+        conversionName = sel.options[sel.selectedIndex].text;
+        document.getElementById("conversion-name").innerHTML = conversionName + " :";
+        document.getElementById("convert").addEventListener('click', function() {
+            inputValue = document.getElementById("input-value").value;
+            document.getElementById("output-value").value = dec2hex(inputValue); // printing output
+        });
+    }else if(selected === "hex2bin") { // Hexadecimal to Binary
+        conversionName = sel.options[sel.selectedIndex].text;
+        document.getElementById("conversion-name").innerHTML = conversionName + " :";
+        document.getElementById("convert").addEventListener('click', function() {
+            inputValue = document.getElementById("input-value").value;
+            inputValue = inputValue.toString();
+            document.getElementById("output-value").value = hex2bin(inputValue);
+        });
+    }else if(selected === "bin2hex") { //Binary to Hexadecimal 
+        conversionName = sel.options[sel.selectedIndex].text;
+        document.getElementById("conversion-name").innerHTML = conversionName + " :";
+        document.getElementById("convert").addEventListener('click', function() {
+            inputValue = document.getElementById("input-value").value;
+            document.getElementById("output-value").value = bin2hex(inputValue);
+        });
+    }
+});
+
+document.querySelector("#copy").addEventListener('click', copy); // to copy output value
---
+++ script/methods.js
@@ -0,0 +1,39 @@
+// Binary to Decimal
+function bin2dec(bin) {
+    return Number(parseInt(bin,2).toString(10));
+}
+
+// Hexadecimal to Decimal
+function hex2dec(hex) {
+    return parseInt(hex,16).toString(10);
+}
+
+// Decimal to Binary
+function dec2bin(dec) {
+    return Number((dec>>>0).toString(2));
+}
+
+// Decimal to Hexadecimal
+function dec2hex (dec) {
+    return parseInt(dec, 10).toString(16);
+}
+
+// Binary to Hexadecimal 
+function bin2hex (bin) {
+    return parseInt(bin, 2).toString(16);
+}
+
+// Hexadecimal to Binary
+function hex2bin (hex) {
+    return Number(parseInt(hex, 16).toString(2));
+}
+
+// function to make copy button
+function copy() {
+    var copyText = document.querySelector("#output-value");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+
+
